1855de90a1de72e262d73d60aa48a19f1f4a2d46
Giygas <gustavorossich@gmail.com>
2025-10-21 23:12:23 +0200

    removed image from snacks

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -7,7 +7,7 @@ return {
 			-- your configuration comes here
 			-- or leave it empty to use the default settings
 			-- refer to the configuration section below
-			image = { enabled = true },
+			image = { enabled = false },
 			input = { enabled = false },
 			picker = { enabled = false },
 			explorer = { enabled = false },
