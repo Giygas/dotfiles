e46fd0a339b2d857e9991c0ce98c3949e4f0733a
Giygas <gustavorossich@gmail.com>
2025-10-27 03:23:39 +0100

    nvim styling

--- dot_config/nvim/lua/core/colorscheme.lua
+++ dot_config/nvim/lua/core/colorscheme.lua
@@ -8,8 +8,8 @@ function ColorMyPencils(color)
 
 	-- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#000000" })
 
-	-- -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#3c3836" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 	-- vim.cmd([[hi NvimTreeNormal guibg=NONE]])
 
 	-- vim.cmd([[
