508eec7abc4ec08af092440af34f6e0ed440f35c
Giygas <giygas.dev@pm.me>
2026-04-11 16:52:54 +0200

    updates

---
+++ dot_config/wezterm/colorscheme.lua
@@ -0,0 +1,35 @@
+-- vi:set filetype=lua
+-- Generated with wallust
+
+local config = {}
+
+config.colors = {
+	cursor_bg = "#6E459B",
+	cursor_border = "#B7C8E0",
+	background = "#211829",
+	foreground = "#B7C8E0",
+
+	ansi = {
+		"#362C3F",
+		"#6E459B",
+		"#4771A9",
+		"#6E459B",
+		"#681957",
+		"#132240",
+		"#264A7D",
+		"#90A8CB",
+	},
+
+	brights = {
+		"#65768E",
+		"#935CCF",
+		"#5E97E1",
+		"#8A2174",
+		"#6E459B",
+		"#192D55",
+		"#3262A7",
+		"#90A8CB",
+	},
+}
+
+return config
